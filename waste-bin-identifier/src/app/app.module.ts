93cf84ebd4778bcdbdc800fbbcf0279cbbcad7a8
Anjal Afghan <anjal.afghan93@gmail.com>
2019-01-17 15:57:29 +0530

    yash's src push

--- waste-bin-identifier/src/app/app.module.ts
+++ waste-bin-identifier/src/app/app.module.ts
@@ -4,12 +4,19 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
+import { Camera } from '@ionic-native/camera';
 import { MyApp } from './app.component';
+import { WelcomePage } from '../pages/welcome/welcome';
+import { LoginPage } from '../pages/login/login';
+import { RegisterPage } from '../pages/register/register';
 import { HomePage } from '../pages/home/home';
 
 @NgModule({
   declarations: [
     MyApp,
+    WelcomePage,
+    LoginPage,
+    RegisterPage,
     HomePage
   ],
   imports: [
@@ -19,11 +26,15 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
+    WelcomePage,
+    LoginPage,
+    RegisterPage,
     HomePage
   ],
   providers: [
     StatusBar,
     SplashScreen,
+    Camera,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
