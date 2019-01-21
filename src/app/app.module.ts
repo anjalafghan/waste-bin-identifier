f8bb424ec9537ba20086bad7efdb06a79496d8fe
yashsarjekar <yashsarjekar007@gmail.com>
2019-01-21 15:48:38 +0530

    Camera improvement

---
+++ src/app/app.module.ts
@@ -0,0 +1,45 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+
+import { Camera } from '@ionic-native/camera';
+import { MyApp } from './app.component';
+import { WelcomePage } from '../pages/welcome/welcome';
+import { LoginPage } from '../pages/login/login';
+import { RegisterPage } from '../pages/register/register';
+import { HomePage } from '../pages/home/home';
+import { IonicImageViewerModule } from 'ionic-img-viewer';
+import { AuthServiceProvider } from '../providers/auth-service/auth-service';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    WelcomePage,
+    LoginPage,
+    RegisterPage,
+    HomePage
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp),
+    IonicImageViewerModule
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    WelcomePage,
+    LoginPage,
+    RegisterPage,
+    HomePage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    Camera,
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    AuthServiceProvider
+  ]
+})
+export class AppModule {}
